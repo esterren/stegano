bb3943791af36782601d400c1ca8238f03457c25
esterren <renato.estermann@gmail.com>
2012-03-19 10:47:46 +0100

    ref #2 ContollerTest and first commit with ServiceHook

--- src/main/java/ch/zhaw/swp2/stegano/controller/Controller.java
+++ src/main/java/ch/zhaw/swp2/stegano/controller/Controller.java
@@ -2,4 +2,7 @@ package ch.zhaw.swp2.stegano.controller;
 
 public class Controller {
 
+	public static void main(String[] args) {
+
+	}
 }
--- src/test/java/ch/zhaw/swp2/stegano/controller/ControllerTest.java
+++ src/test/java/ch/zhaw/swp2/stegano/controller/ControllerTest.java
@@ -1,11 +1,15 @@
 package ch.zhaw.swp2.stegano.controller;
 
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class ControllerTest {
+
+	private Controller _ctrl;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -13,10 +17,16 @@ public class ControllerTest {
 
 	@Before
 	public void setUp() throws Exception {
+		_ctrl = new Controller();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	@Test
+	public void startController() {
+		assertTrue(true);
+
+	}
 }
