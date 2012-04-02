5056fc28d30b1304fdc3924bebaa0726b10f0a39
NikiH <niki.hausammann@gmail.com>
2012-04-02 14:49:15 +0200

    CRC Check

--- src/main/java/ch/zhaw/swp2/stegano/model/CRCFactory.java
+++ src/main/java/ch/zhaw/swp2/stegano/model/CRCFactory.java
@@ -1,5 +1,25 @@
 package ch.zhaw.swp2.stegano.model;
 
-public final class CRCFactory {
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.zip.CRC32;
 
+public final class CRCFactory {
+	
+	public byte[] getCRC(byte[] data) throws IOException {
+		
+		CRC32 crc = new CRC32();
+		crc.update(data);
+		
+		//convert crc from long to byte-array
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();  
+		DataOutputStream dos = new DataOutputStream(bos);  
+		dos.writeLong(crc.getValue());  
+		dos.flush();
+		
+		byte[] crcB = bos.toByteArray();
+		
+		return crcB;
+	};
 }
