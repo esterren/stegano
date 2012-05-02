e4753058407901a3e47b2421f4e49bcf321580b4
NikiH <niki.hausammann@gmail.com>
2012-05-03 00:46:49 +0200

    in audio start

---
+++ src/main/java/ch/zhaw/swp2/stegano/model/InAudioStrategy.java
@@ -0,0 +1,21 @@
+package ch.zhaw.swp2.stegano.model;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class InAudioStrategy implements SteganoStrategy {
+
+	@Override
+	public BufferedImage runHide(File inBaseFile, File inHiddenFile,
+			byte pollution) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File runSeek(File inModBaseFile) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
